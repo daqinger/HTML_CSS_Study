c6aa8509e496506242f3f92e6b4912135c0368e2
daqinger <1369462110@qq.com>
2019-12-12 10:36:30 +0800

    第一次提交

---
+++ message.js
@@ -0,0 +1,30 @@
+function message() {
+    alert("hi");
+}
+message();
+
+/*
+ *这是注释部分
+ */
+
+function test() {
+    var problem = "问题";
+    fond = false;
+    age = 29;
+    alert(problem + fond);
+    // alert (problem);
+    // alert(fond);
+    // alert(age);
+    // alert(typeof age);
+}
+test();
+
+function test2(a, b) {
+    alert(a + b);
+}
+test2(3, 2);
+
+function sayhi(name, message) {
+    alert(name + message);
+}
+var color = new Array(20);
